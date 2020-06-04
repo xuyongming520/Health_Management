528ff2cca7ba1d5f6cd529f0944ab5469ce5de8a
xuyongming520 <1692679304@qq.com>
2020-06-04 13:36:59 +0800

    ending

--- src/main/java/cn/healthy/manage/service/impl/ProductServicelImpl.java
+++ src/main/java/cn/healthy/manage/service/impl/ProductServicelImpl.java
@@ -50,10 +50,6 @@ public class ProductServicelImpl extends ServiceImpl<ProductMapper,Product> impl
 
 
     public IPage<Product> selectProductList(ProductPageRequest request) {
-        // 不进行 count sql 优化，解决 MP 无法自动优化 SQL 问题，这时候你需要自己查询 count 部分
-        // page.setOptimizeCountSql(false);
-        // 当 total 为小于 0 或者设置 setSearchCount(false) 分页插件不会进行 count 查询
-        // 要点!! 分页返回的对象与传入的对象是同一个
         QueryWrapper<Product> queryWrapper = new QueryWrapper<>();
         LambdaQueryWrapper<Product> lambdaQueryWrapper = queryWrapper.lambda();
         if (!StringUtils.isEmpty(request.getName())){
